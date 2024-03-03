ce808752f3d7830921cddbf1513e025d436afc1c
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-03-03 23:51:10 +0500

    fix: add fields in UserModel

--- AlgoLibrary/Models/UserModel.cs
+++ AlgoLibrary/Models/UserModel.cs
@@ -1,6 +1,36 @@
-﻿namespace AlgoLibrary.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlgoLibrary.Models
 {
     public class UserModel
     {
+        private int userId;
+        private string login;
+        private int password;
+        private string role;
+        [Key]
+        public int UserId
+        {
+            get { return userId; }
+            set { userId = value; }
+        }
+
+        public string Login
+        {
+            get { return login; }
+            set { login = value; }
+        }
+
+        public int Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public string Role
+        {
+            get { return role; } 
+            set { role = value; }
+        }
     }
 }
