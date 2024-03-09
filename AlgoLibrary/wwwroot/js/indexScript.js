6b28741cf8212050a4848d0ca67c686d8e58fd82
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-03-09 21:41:21 +0500

    feat: add code highlighter and styles for articles menu

--- AlgoLibrary/wwwroot/js/indexScript.js
+++ AlgoLibrary/wwwroot/js/indexScript.js
@@ -1,11 +1,14 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
-    // Получить список всех статей вместе с подключенными темами
     var articlesWithThemes = window.articlesWithThemes;
 
-    // Создать объект для хранения статей, сгруппированных по темам
     var groupedArticles = {};
 
-    // Группировать статьи по темам
+    articlesWithThemes.sort(function (a, b) {
+        return a.theme.orderNumber - b.theme.orderNumber;
+    });
+
+    console.log(articlesWithThemes);
+
     articlesWithThemes.forEach(function (article) {
         if (!groupedArticles.hasOwnProperty(article.theme.name)) {
             groupedArticles[article.theme.name] = [];
@@ -13,7 +16,14 @@
         groupedArticles[article.theme.name].push(article);
     });
 
-    // Отобразить статьи, сгруппированные по темам
+    Object.keys(groupedArticles).forEach(function (themeName) {
+        groupedArticles[themeName].sort(function (a, b) {
+            return a.orderNumber - b.orderNumber;
+        });
+    });
+
+    console.log(groupedArticles);
+
     var container = document.getElementById("articlesList");
     Object.keys(groupedArticles).forEach(function (themeName) {
         var themeArticles = groupedArticles[themeName];
@@ -22,15 +32,16 @@
         themeHeader.textContent = themeName;
         themeItem.appendChild(themeHeader);
         var articlesList = document.createElement("ul");
+
         themeArticles.forEach(function (article) {
             var articleItem = document.createElement("li");
             var articleLink = document.createElement("a");
             articleLink.textContent = article.name;
-            articleLink.href = "#"; // Здесь можно указать ссылку на действительную страницу статьи
-            articleLink.dataset.articleId = article.articleId; // Запоминаем идентификатор статьи
+            articleLink.href = "#";
+            articleLink.dataset.articleId = article.articleId;    
             articleLink.addEventListener("click", function (event) {
-                event.preventDefault(); // Отменяем действие по умолчанию (переход по ссылке)
-                loadArticle(article.articleId); // Загружаем текст статьи по идентификатору
+                event.preventDefault();
+                loadArticle(article.articleId);
             });
             articleItem.appendChild(articleLink);
             articlesList.appendChild(articleItem);
